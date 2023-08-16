cb27c7db8c16084aa724481afdc33aa016ca6cf0
Leonardo Gomes <leogomes.pereira03@gmail.com>
2023-08-16 15:19:37 -0300

    deixando só os arquivos de react

--- nativetamagui/packages/app/provider/ToastViewport.tsx
+++
@@ -1,7 +0,0 @@
-import { ToastViewport as ToastViewportOg } from '@my/ui'
-import { useSafeAreaInsets } from 'react-native-safe-area-context'
-
-export const ToastViewport = () => {
-  const { top, right, left, bottom } = useSafeAreaInsets()
-  return <ToastViewportOg top={top + 5} left={left} right={right} bottom={bottom + 200} />
-}
